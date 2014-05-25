c8ad729ad097b29b16f2933ed00ae3b87c97cdc4
nullchimpothesis <rhiznay@yahoo.com>
2014-05-25 12:58:46 -0700

    intermediate submission

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------
+#
+#run_analysis.R
+#project for Getting and Cleaning Data
+#Get the data set
+
+
+
+#Merges the training and test sets to create one data set
+
+#Activity Name
+
+#Creates a second, independent tidy data set with the average
+s
